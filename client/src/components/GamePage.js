2f3848bd71327b185f8cc337598fc6a47dd3988a
ldv432 <ldv432@gmail.com>
2024-12-31 12:07:41 -0700

    GAME FINALLY RUNNING ON PAGEgit add .git add .

--- client/src/components/GamePage.js
+++ client/src/components/GamePage.js
@@ -1,7 +1,23 @@
-import React from "react";
-
 const GamePage = () => {
-  return <div>Welcome to the Game Page!</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "calc(100vh - 60px)", // Adjust this based on your navbar height
+        marginTop: "60px", // Space for the navbar
+      }}
+    >
+      <iframe
+        src="https://ldv432.github.io/iras-quest-game/"
+        title="Pygbag Game"
+        width="1400px" // Set the desired width
+        height="1000px" // Set the desired height
+        style={{ border: "none" }}
+      ></iframe>
+    </div>
+  );
 };
 
 export default GamePage;
